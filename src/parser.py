9159d9c27bb05cff0231ce5b92e6c966e884ae48
Théo Verhelst <tverhels@ulb.ac.be>
2018-11-01 18:30:56 +0100

    Parse fields from text file in the right order

--- src/parser.py
+++ src/parser.py
@@ -4,6 +4,6 @@ from classes import Task
 def parse_system(input_file):
     """Given a properly formatted input file, it returns a list of tasks."""
     reader = csv.DictReader(open(input_file),
-                            fieldnames=['offset', 'period', 'wcet'],
+                            fieldnames=['offset', 'wcet', 'period'],
                             delimiter=';')
     return [Task(int(row['offset']), int(row['wcet']), int(row['period']), i) for i, row in enumerate(reader)]
