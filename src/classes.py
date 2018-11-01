02022b6c99cc7020b7ff397496d7a7841b9d0015
Théo Verhelst <tverhels@ulb.ac.be>
2018-11-01 15:42:27 +0100

    Add classes for Job and Task

---
+++ src/classes.py
@@ -0,0 +1,15 @@
+class Task:
+    def __init__(self, offset, wcet, period, index):
+        self.offset = offset
+        self.wcet = wcet
+        self.period = period
+        self.index = index
+        self.job_counter = 0
+
+class Job:
+    def __init__(self, task):
+        self.task = task
+        self.remaining_execution_time = task.wcet
+        self.time_to_deadline = task.period
+        self.name = "T" + str(task.index) + "J" + str(task.job_counter)
+        task.job_counter += 1
