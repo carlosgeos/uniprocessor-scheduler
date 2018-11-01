ad4276c4d299464aa22bc15b5036b2466f77200c
Théo Verhelst <tverhels@ulb.ac.be>
2018-11-01 18:31:53 +0100

    Implement log printing close to that of statement

--- src/simulation.py
+++ src/simulation.py
@@ -3,15 +3,20 @@ from classes import Job
 def simulate(scheduler, tasks, start, stop):
     jobs = []
     preemption_count = 0
+    # The job currently scheduled
     current = None
+    # The time since the current job has been lastly started
+    # It is used only for printing
+    uptime_current = 0
     for t in range(start, stop):
         deadline_misses = []
         job_arrivals = []
 
         if current != None:
             current.remaining_execution_time -= 1
+            uptime_current += 1
             if current.remaining_execution_time <= 0:
-                jobs.remove(job)
+                jobs.remove(current)
 
         # Decrease time to deadline of all jobs
         for job in jobs:
@@ -24,7 +29,7 @@ def simulate(scheduler, tasks, start, stop):
 
         # Add a new job if it's realase time for a task
         for task in tasks:
-            if t - task.offset % task.period == 0:
+            if (t - task.offset) % task.period == 0:
                 new_job = Job(task)
                 job_arrivals.append(new_job)
                 jobs.append(new_job)
@@ -35,8 +40,10 @@ def simulate(scheduler, tasks, start, stop):
         # Take the jobs with highest priority
         scheduled = jobs[0] if len(jobs) > 0 else None
         # If we do a preemption, increment the counter
-        if scheduled != current and scheduled != None:
+        if scheduled != current and current != None:
             preemption_count += 1
+            print(t - uptime_current, "-", t, ": ", current)
+            uptime_current = 0
         # Schedule the job
         current = scheduled
 
@@ -45,9 +52,6 @@ def simulate(scheduler, tasks, start, stop):
             print(t, ": Job ", deadline_miss, " misses a deadline", sep="")
         for job_arrival in job_arrivals:
             print(t, ": Arrival of job ", job_arrival, sep="")
-        # TODO: print the whole scheduled execution at once rather than
-        # one line per time unit
-        print(t, "-", t + 1, ": ", current, sep="")
 
 class EdfScheduler:
     def sort_jobs(self, jobs):
