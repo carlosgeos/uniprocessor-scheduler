b433fc015002812bbbd5377ae6ec42c166e36e01
Théo Verhelst <tverhels@ulb.ac.be>
2018-11-01 17:23:46 +0100

    Use Task class in parser

--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,9 @@
 import csv
-
+from classes import Task
 
 def parse_system(input_file):
-    """Given a properly formatted input file, it returns an array of
-    dictionnaries (one for each task) with keys 'offset', 'period' and
-    'wcet'
-
-    """
+    """Given a properly formatted input file, it returns a list of tasks."""
     reader = csv.DictReader(open(input_file),
                             fieldnames=['offset', 'period', 'wcet'],
                             delimiter=';')
-    return list(reader)
+    return [Task(row['offset'], row['wcet'], row['period'], i) for i, row in enumerate(reader)]
--- src/project.py
+++ src/project.py
@@ -7,8 +7,8 @@ SYSTEM_FILE = os.path.join(os.path.dirname(__file__), 'data/system.txt')
 
 
 def main():
-    system_description = parse_system(SYSTEM_FILE)
-    print(system_description)
+    tasks = parse_system(SYSTEM_FILE)
+    print(tasks)
 
-
-main()
+if __name__ == "__main__":
+    main()
