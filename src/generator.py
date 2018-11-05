9a675d80577effde0ff1998424e845db98b7f1a2
Carlos Requena <crequena@ulb.ac.be>
2018-11-05 09:10:49 +0100

    add generator.py function signatures / structure (not done)

--- src/generator.py
+++ src/generator.py
@@ -1,2 +1,22 @@
+import random
+
+
+def deviate_load(elem, coll):
+    """Randomize average load for each of the tasks
+
+    """
+    pass
+
+
+def load_to_wcet_period_ratio(elem):
+    """Establish an approximate ratio WCET / Period that satisfies the
+    load factor of the task given as input in elem.
+
+    """
+    pass
+
+
 def generate_tasks(args):
-    print(args.load_factor)
+    params = vars(args)
+    avg_load = params['load_factor'] / params['tasks']
+    print(avg_load)
